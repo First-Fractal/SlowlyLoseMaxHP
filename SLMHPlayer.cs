34e927de6e85247e15c3fa9572210000b2c2bb26
First Fractal <firstfractal512@gmail.com>
2024-01-27 19:24:45 -0600

    made it remove life fruit and then life crystals

--- SLMHPlayer.cs
+++ SLMHPlayer.cs
@@ -7,7 +7,7 @@ namespace SlowlyLoseMaxHP
     internal class SLMHPlayer : ModPlayer
     {
         //vars for counting down the cooldown for losing max hp
-        static int cooldownMax = ffFunc.TimeToTick(10);
+        static int cooldownMax = ffFunc.TimeToTick(1);
         static int cooldown = cooldownMax;
 
         //vars for counting down the cooldown for losing max hp
@@ -17,11 +17,22 @@ namespace SlowlyLoseMaxHP
             if (cooldown > 0)
             {
                 cooldown--;
-                Console.WriteLine(cooldown);
             } else
             {
-                //remove one life crystal from the player, decreasing there max hp
-                Player.ConsumedLifeCrystals--;
+                Console.WriteLine($"The Player {Player.name} has {Player.ConsumedLifeCrystals} Life Crystals and {Player.ConsumedLifeFruit} Life Frults");
+
+                //check if the player has one life fruit
+                if (Player.ConsumedLifeFruit > 0)
+                {
+                    //remove one life fruit from the player, decreasing there max hp
+                    Player.ConsumedLifeFruit--;
+                }
+                //check if the player has one life crystal
+                else if (Player.ConsumedLifeCrystals > 0)
+                {
+                    //remove one life crystal from the player, decreasing there max hp
+                    Player.ConsumedLifeCrystals--;
+                }
 
                 //reset the cooldown
                 cooldown = cooldownMax;
