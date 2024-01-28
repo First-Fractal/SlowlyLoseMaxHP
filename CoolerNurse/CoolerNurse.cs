b7233d750592b756980ac18197a4fd397c5ea34f
First Fractal <firstfractal512@gmail.com>
2024-01-28 14:32:55 -0600

    made some of the stats the same as the vanilla nurse

--- CoolerNurse/CoolerNurse.cs
+++ CoolerNurse/CoolerNurse.cs
@@ -34,15 +34,23 @@ namespace SlowlyLoseMaxHP.CoolerNurse
             //say what frames are for attacking
             NPCID.Sets.AttackFrameCount[Type] = 4;
 
-            NPCID.Sets.DangerDetectRange[Type] = 700; // The amount of pixels away from the center of the NPC that it tries to attack enemies.
-            NPCID.Sets.AttackType[Type] = 0; // The type of attack the Town NPC performs. 0 = throwing, 1 = shooting, 2 = magic, 3 = melee
-            NPCID.Sets.AttackTime[Type] = 90; // The amount of time it takes for the NPC's attack animation to be over once it starts.
-            NPCID.Sets.AttackAverageChance[Type] = 30; // The denominator for the chance for a Town NPC to attack. Lower numbers make the Town NPC appear more aggressive.
-            NPCID.Sets.HatOffsetY[Type] = 0; // For when a party is active, the party hat spawns at a Y offset.
-            NPCID.Sets.ShimmerTownTransform[NPC.type] = true; // This set says that the Town NPC has a Shimmered form. Otherwise, the Town NPC will become transparent when touching Shimmer like other enemies.
+            //set the offset for the party hat
+            NPCID.Sets.HatOffsetY[Type] = 0;
+
+            // The amount of pixels away from the center of the NPC that it tries to attack enemies.
+            NPCID.Sets.DangerDetectRange[Type] = NPCID.Sets.DangerDetectRange[NPCID.Nurse];
+
+            // The type of attack the Town NPC performs. 0 = throwing, 1 = shooting, 2 = magic, 3 = melee
+            NPCID.Sets.AttackType[Type] = NPCID.Sets.DangerDetectRange[NPCID.Nurse];
+
+            // The amount of time it takes for the NPC's attack animation to be over once it starts.
+            NPCID.Sets.AttackTime[Type] = NPCID.Sets.DangerDetectRange[NPCID.Nurse];
+
+            // The denominator for the chance for a Town NPC to attack. Lower numbers make the Town NPC appear more aggressive.
+            NPCID.Sets.AttackAverageChance[Type] = NPCID.Sets.DangerDetectRange[NPCID.Nurse];
 
             //tell the game that the cooler nurse has a different sprite for shimmer
-            NPCID.Sets.ShimmerTownTransform[Type] = true; // Allows for this NPC to have a different texture after touching the Shimmer liquid.
+            NPCID.Sets.ShimmerTownTransform[Type] = true; 
 
             //save the different varation of the cooler nurse
             NPCProfile = new Profiles.StackedNPCProfile(
