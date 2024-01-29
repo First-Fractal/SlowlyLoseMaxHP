9e17e92e8e2d69dfff0c27cf52e0882d8fb1a912
First Fractal <firstfractal512@gmail.com>
2024-01-28 19:57:54 -0600

    have the shop text appear on screen

---
+++ SLMHModSystem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Terraria.UI;
+using Terraria;
+using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+
+namespace SlowlyLoseMaxHP
+{
+    internal class SLMHModSystem : ModSystem
+    {
+        internal UserInterface nurseInterface;
+        internal SLMHUIState nurseUI;
+        private GameTime lastGameTime;
+        public override void Load()
+        {
+            if (!Main.dedServ)
+            {
+                nurseInterface = new UserInterface();
+                nurseUI = new SLMHUIState();
+                nurseUI.Activate();
+            }
+        }
+
+        public override void UpdateUI(GameTime gameTime)
+        {
+            lastGameTime = gameTime;
+            if (nurseInterface?.CurrentState != null)
+            {
+                nurseInterface.Update(gameTime);
+            }
+
+            nurseInterface?.SetState(nurseUI);
+            base.UpdateUI(gameTime);
+        }
+
+        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
+        {
+            int mouseTextIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Mouse Text"));
+            if (mouseTextIndex != -1)
+            {
+                layers.Insert(mouseTextIndex, new LegacyGameInterfaceLayer(
+                    "Boss Adrenaline Counter",
+                    delegate
+                    {
+                        if (lastGameTime != null && nurseInterface?.CurrentState != null)
+                        {
+                            nurseInterface.Draw(Main.spriteBatch, lastGameTime);
+                        }
+                        return true;
+                    },
+                    InterfaceScaleType.UI));
+            }
+            base.ModifyInterfaceLayers(layers);
+        }
+    }
+}
---
+++ SLMHUIState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.UI;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent;
+using Terraria.UI.Chat;
+using Terraria.Localization;
+
+namespace SlowlyLoseMaxHP
+{
+    internal class SLMHUIState : UIState
+    {
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            ChatManager.DrawColorCodedString(spriteBatch: spriteBatch, FontAssets.MouseText.Value, Language.GetTextValue("LegacyInterface.28"),
+                new Vector2 (Main.screenWidth/2, Main.screenHeight/2), new Color(228, 206, 114, Main.mouseTextColor / 2),
+                0f, new Vector2(1, 1), new Vector2(1, 1));
+
+            base.Draw(spriteBatch);
+        }
+    }
+}
